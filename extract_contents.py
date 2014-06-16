aab2b1562a67a6615017fdfca2b23ca232f6ba5b
hxiao <hxiao@cs.helsinki.fi>
2014-06-16 16:29:06 +0300

    using doc id to do the content extraction work

--- extract_contents.py
+++ extract_contents.py
@@ -4,7 +4,43 @@ from time import time
 
 from setting import MYSQL_CONN_SETTING
 
-def main(offset, limit, extractor_name = 'ArticleExtractor'): 
+conn = MySQLdb.connect(**MYSQL_CONN_SETTING)
+def extract_by_db_ids(ids, extractor_name = 'ArticleExtractor'):
+    """
+    Given a bunch of ids and extract the content of documents specified by the ids
+    """
+    c = conn.cursor()
+    counter = 0
+    frequency = 10
+    start = time()
+
+    for id in ids:
+        c.execute('SELECT id, uncompress(crawled_html) FROM webpage WHERE id=%s', (id, ))
+        row = c.fetchone()
+
+        id, html = row
+        print id
+        if html is None:
+            print 'html is corrupted'
+            continue
+        
+        try:
+            extractor = Extractor(extractor=extractor_name, html=html)
+            content = extractor.getText()
+        except:
+            print 'Error occured during content extraction'
+            continue
+                    
+        c.execute('UPDATE webpage set processed_content = %s where id=%s', (content, id))
+        counter += 1
+        
+        if counter % frequency == 0:
+            print 'procssed %d items, used %f seconds' %(frequency, time() - start)
+            start = time()
+            conn.commit()
+            counter = 0
+            
+def main(offset = 0, limit = 0, extractor_name = 'ArticleExtractor'): 
     upd_conn = MySQLdb.connect(**MYSQL_CONN_SETTING)
     
     MYSQL_CONN_SETTING['cursorclass'] = MySQLdb.cursors.SSCursor
@@ -46,9 +82,15 @@ def main(offset, limit, extractor_name = 'ArticleExtractor'):
             upd_conn.commit()
             counter = 0
 
+
 if __name__ == "__main__":
     import sys
-    offset = sys.argv[1]
-    limit = sys.argv[2]
-    main(int(offset), int(limit))
-
+    id_file_path = sys.argv[1]
+    with open(id_file_path, 'r') as f:
+        ids = (int(id_str.strip()) 
+               for id_str in f.readlines())
+        extract_by_db_ids(ids)
+        
+    # offset = sys.argv[1]
+    # limit = sys.argv[2]
+    # main(int(offset), int(limit)
